29c63926dd5a0893bd60b83389ebae7b4fca94f6
JSjump <yanbin.zhu1@china.zhaogang.com>
2022-03-06 13:12:45 +0800

    feat(*): 完成

--- contracts/Score.sol
+++ contracts/Score.sol
@@ -1,22 +1,65 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 import "hardhat/console.sol";
 
 contract Score {
-    string private greeting;
+    using SafeMath for uint256;
+    // immutable ,constant.   不可修改，  constant 编译时确定。  immutable部署时确定
+    address internal immutable teacher;
+    /**
+     * 记账本
+     */
+    mapping(address => uint256) public scores;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    constructor() {
+        teacher = msg.sender;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    // 登记分数
+    function setScore(address _addr, uint256 _score)
+        external
+        scoreLimit(_score)
+        onlyTeacher
+    {
+        scores[_addr] = _score;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    // 加分
+    function scoreAdd(address _addr, uint256 _score)
+        external
+        scoreLimit(_score)
+        onlyTeacher
+    {
+        scores[_addr] = scores[_addr].add(_score);
+    }
+
+    // 减分
+    function scoreSub(address _addr, uint256 _score)
+        external
+        scoreLimit(_score)
+        onlyTeacher
+    {
+        scores[_addr] = scores[_addr].sub(_score);
+    }
+
+    // 查分
+    function getScore(address _addr) external view returns (uint256) {
+        return scores[_addr];
+    }
+
+    // 分数不能 > 100 , 小于 0
+    modifier scoreLimit(uint256 _score) {
+        require(_score > 0, "must > 100");
+        require(_score <= 100, "can't > 100");
+        _;
+    }
+
+    // 只能老师修改分数
+    modifier onlyTeacher() {
+        require(teacher == msg.sender, "onlyTeacher");
+        _;
     }
 }
--- contracts/Teacher.sol
+++ contracts/Teacher.sol
@@ -3,20 +3,25 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+import "./Score.sol";
+
+interface IScore {
+    function setScore(address _addr, uint256 _score) external;
+
+    function scoreAdd(address _addr, uint256 _score) external;
+
+    function scoreSub(address _addr, uint256 _score) external;
+}
+
 contract Teacher {
-    string private greeting;
+    address public scoreAddress;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    function createScoreBook() external {
+        scoreAddress = address(new Score());
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    //  设置学生分数
+    function setStudentScore(address _student, uint256 _score) external {
+        IScore(scoreAddress).setScore(_student, _score);
     }
 }
